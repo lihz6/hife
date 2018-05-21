9b00854b3e2dbc3324adfe6556b675da36f9668e
Li Hongzhang <poodll@163.com>
2018-05-21 17:49:10 +0800

    typescript

--- range.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = (length) => {
-    let index = 0, list = [];
-    while (list.push(index++) < length)
-        ;
-    return list;
-};
